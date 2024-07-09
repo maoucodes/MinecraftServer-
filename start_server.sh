6999ea43a9e9252d4ca30029cdad258a42252e06
maoucodes <162734089+maoucodes@users.noreply.github.com>
2024-07-09 13:57:17 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -36,6 +36,12 @@ download() {
     echo ""
     echo "Thank you for agreeing, the download will now begin."
     echo ""
+    echo -e "${BLUE}Downloading Java...${NC}"
+    curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
+    jabba install adoptiumjdk@21.0.3=tgz+https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.3%2B9/OpenJDK21U-jdk_x64_linux_hotspot_21.0.3_9.tar.gz
+    jabba use adoptiumjdk@21.0.3
+    echo -e "${GREEN}Java is installed.${NC}"
+    java -version
 
     case "${SERVER,,}" in
     purpur)
